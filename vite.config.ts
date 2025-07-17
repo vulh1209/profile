8823f218b3a71858cf346eb96e0a5f7be1ffb9ec
Hoàng Vũ <vu.le@sipher.xyz>
2025-07-17 22:35:05 +0700

    Update vite.config.ts to enhance server and build configurations
    - Added filesystem strict mode configuration to server settings for improved file system handling.
    - Configured esbuild loader for TypeScript and JSX files to streamline the build process.

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,14 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true,
+    fs: {
+      strict: false,
+    },
+  },
+  esbuild: {
+    loader: "tsx",
+    include: /src\/.*\.[tj]sx?$/,
+    exclude: [],
   },
   build: {
     outDir: "build",
