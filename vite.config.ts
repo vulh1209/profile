12f452b9d9de161618492148a1e7d38a6485c5a3
Hoàng Vũ <vu.le@sipher.xyz>
2025-07-17 22:13:16 +0700

    Add dotenv dependency and configure environment variables
    - Added dotenv package to manage environment variables.
    - Updated package.json and package-lock.json to include dotenv version 17.2.0.
    - Configured dotenv in vite.config.ts to load environment variables from .env.local file.
    - Enhanced server configuration based on environment settings.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: ".env.local",
+});
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -13,6 +19,14 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true,
+    ...(process.env.LOCAL !== "true"
+      ? {
+          headers: {
+            "Content-Type": "application/javascript",
+          },
+          middlewareMode: false,
+        }
+      : {}),
   },
   build: {
     outDir: 'dist',
